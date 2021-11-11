9f27f5c6ce0e2c88b0c3e9c6bf7060693c162c63
kablu mandal <kablumndl546@gmail.com>
2021-11-11 17:08:49 +0530

    ok

---
+++ nsdl-edustakhat-config/src/test/java/com/nsdl/edustakhat/conf/EdastakhatConfAppTests.java
@@ -0,0 +1,13 @@
+package com.nsdl.edustakhat.conf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdastakhatConfAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
